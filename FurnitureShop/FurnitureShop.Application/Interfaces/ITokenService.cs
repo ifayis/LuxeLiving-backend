8cec88deb178445008b17f82b3db98aed45ba9f8
Fayis KV <mohammedfayis135@gmail.com>
2025-12-22 11:29:17 +0530

    day3

---
+++ FurnitureShop/FurnitureShop.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using FurnitureShop.Domain.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Application.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateToken(User user);
+    }
+}
