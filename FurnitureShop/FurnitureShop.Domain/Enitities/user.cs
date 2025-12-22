f493f53131e0027953fe92f61a48dac8029b05d0
Fayis KV <mohammedfayis135@gmail.com>
2025-12-22 15:56:38 +0530

    day3

--- FurnitureShop/FurnitureShop.Domain/Enitities/user.cs
+++ FurnitureShop/FurnitureShop.Domain/Enitities/user.cs
@@ -8,7 +8,7 @@ namespace FurnitureShop.Domain.Enitities
 {
         public class User
         {
-            public int Id { get; set; }
+            public Guid Id { get; set; }
             public string FullName { get; set; } = string.Empty;
             public string Email { get; set; } = string.Empty;
             public string PasswordHash { get; set; } = string.Empty;
