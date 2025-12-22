00e9d1456c130693b7912791fd75796d0ce2cffe
Fayis KV <mohammedfayis135@gmail.com>
2025-12-22 09:48:03 +0530

    day1

---
+++ FurnitureShop/FurnitureShop/Properties/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using FurnitureShop.Application.DTOs.Auth;
+using FurnitureShop.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureShop.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequestDto request)
+        {
+            await _authService.RegisterAsync(request);
+            return Ok("User registered successfully");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequestDto request)
+        {
+            var isValid = await _authService.LoginAsync(request);
+
+            if (!isValid)
+                return Unauthorized("Invalid email or password");
+
+            return Ok("Login successful");
+        }
+    }
+}
