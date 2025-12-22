57196ca48313a0820f4c8927f5ee203834757561
Fayis KV <mohammedfayis135@gmail.com>
2025-12-22 10:14:02 +0530

    day2

---
+++ FurnitureShop/FurnitureShop.Application/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using FurnitureShop.Domain.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<Product?> GetProductByIdAsync(int id);
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(int categoryId);
+    }
+}
---
+++ FurnitureShop/FurnitureShop.Application/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using FurnitureShop.Application.Interfaces;
+using FurnitureShop.Domain.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategoryAsync(int categoryId)
+        {
+            return await _productRepository.GetByCategoryIdAsync(categoryId);
+        }
+    }
+}
