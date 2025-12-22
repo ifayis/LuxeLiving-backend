ba95931b2e96cce6c59db58b1466296c7d34741a
Fayis KV <mohammedfayis135@gmail.com>
2025-12-22 10:19:30 +0530

    day2

---
+++ FurnitureShop/FurnitureShop/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using FurnitureShop.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureShop.API.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: api/products/{id}
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+
+            if (product == null)
+                return NotFound("Product not found");
+
+            return Ok(product);
+        }
+
+        // GET: api/products/category/{categoryId}
+        [HttpGet("category/{categoryId:int}")]
+        public async Task<IActionResult> GetByCategory(int categoryId)
+        {
+            var products = await _productService.GetProductsByCategoryAsync(categoryId);
+            return Ok(products);
+        }
+    }
+}
