518194696bde03e0b12d10a4386d8cde159c689b
Fayis KV <mohammedfayis135@gmail.com>
2025-12-23 12:49:27 +0530

    day4

---
+++ FurnitureShop/FurnitureShop/Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿using FurnitureShop.Application.DTOs.Order;
+using FurnitureShop.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FurnitureShop.API.Controllers;
+
+[ApiController]
+[Route("api/orders")]
+[Authorize]
+public class OrdersController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+
+    public OrdersController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpPost("checkout")]
+    public async Task<IActionResult> Checkout(CheckoutRequestDto request)
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        await _orderService.CheckoutAsync(userId, request);
+        return Ok("Order placed successfully");
+    }
+}
