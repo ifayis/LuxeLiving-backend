db68247605433ec98dc4bd8af9cd31e662dbef2f
Fayis KV <mohammedfayis135@gmail.com>
2025-12-29 11:50:29 +0530

    common api response added

---
+++ FurnitureShop/FurnitureShop.Application/common/ApiResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace FurnitureShop.Application.Common;
+
+public class ApiResponse<T>
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public T? Data { get; set; }
+
+    public static ApiResponse<T> Success(T data, string message = "Success", int statusCode = 200)
+    {
+        return new ApiResponse<T>
+        {
+            StatusCode = statusCode,
+            Message = message,
+            Data = data
+        };
+    }
+
+    public static ApiResponse<T> Fail(string message, int statusCode)
+    {
+        return new ApiResponse<T>
+        {
+            StatusCode = statusCode,
+            Message = message,
+            Data = default
+        };
+    }
+}
