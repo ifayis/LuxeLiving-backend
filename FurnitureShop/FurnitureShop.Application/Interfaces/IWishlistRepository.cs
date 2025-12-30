408a8eb0ee04e1788728d122ca86de0753d01950
Fayis KV <mohammedfayis135@gmail.com>
2025-12-30 14:08:53 +0530

    wishlist interface updated

---
+++ FurnitureShop/FurnitureShop.Application/Interfaces/IWishlistRepository.cs
@@ -0,0 +1,14 @@
+﻿using FurnitureShop.Domain.Entities;
+
+namespace FurnitureShop.Application.Interfaces
+{
+    public interface IWishlistRepository
+    {
+        Task<Wishlist?> GetByUserIdAsync(Guid userId);
+        Task<Wishlist?> GetByIdAsync(Guid id);
+        Task AddAsync(Wishlist wishlist);
+        Task SaveChangesAsync();
+        void RemoveItem(WishlistItem item);
+        void RemoveAll(Wishlist wishlist);
+    }
+}
---
+++ FurnitureShop/FurnitureShop.Application/Interfaces/IWishlistService.cs
@@ -0,0 +1,13 @@
+﻿using FurnitureShop.Application.DTOs.Wishlist;
+
+namespace FurnitureShop.Application.Interfaces
+{
+    public interface IWishlistService
+    {
+        Task AddAsync(Guid userId, AddToWishlistRequestDto request);
+        Task<WishlistResponseDto?> GetMyWishlistAsync(Guid userId);
+        Task<WishlistResponseDto?> GetByIdAsync(Guid id);
+        Task RemoveItemAsync(Guid userId, Guid itemId);
+        Task ClearAsync(Guid userId);
+    }
+}
