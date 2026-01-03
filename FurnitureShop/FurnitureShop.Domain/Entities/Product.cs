38b8d7eb5088753135afd183c7bdc9dc6fd85b7f
Fayis KV <mohammedfayis135@gmail.com>
2026-01-03 15:30:41 +0530

    updated

--- FurnitureShop/FurnitureShop.Domain/Entities/Product.cs
+++ FurnitureShop/FurnitureShop.Domain/Entities/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FurnitureShop.Domain.Entities;
 
 namespace FurnitureShop.Domain.Enitities
 {
