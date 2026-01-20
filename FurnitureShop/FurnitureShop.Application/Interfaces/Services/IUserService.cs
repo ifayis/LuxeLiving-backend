f7892dcd5c0b5cd799b9e2d7d43acc0b9afeb123
Fayis KV <mohammedfayis135@gmail.com>
2026-01-20 14:19:04 +0530

    Added Service interface for the usres list

---
+++ FurnitureShop/FurnitureShop.Application/Interfaces/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using FurnitureShop.Application.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Application.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<List<UserResponseDto>> GetAllUsersAsync();
+        Task<UserResponseDto> GetUserByIdAsync(Guid id);
+    }
+}
