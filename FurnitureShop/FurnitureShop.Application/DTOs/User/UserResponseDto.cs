d854fd7afac67faf441b753fbad6060b79193bed
Fayis KV <mohammedfayis135@gmail.com>
2026-01-20 14:14:59 +0530

    Added response items for user details

--- FurnitureShop/FurnitureShop.Application/DTOs/User/UserResponseDto.cs
+++ FurnitureShop/FurnitureShop.Application/DTOs/User/UserResponseDto.cs
@@ -11,6 +11,6 @@ namespace FurnitureShop.Application.DTOs.User
         public Guid Id { get; set; }
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Role { get; set } = string.Empty;
+        public string Role { get; set; } = string.Empty;
     }
 }
