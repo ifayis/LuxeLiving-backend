2533113b5a1547c6699990350416a36911aacc17
Fayis KV <mohammedfayis135@gmail.com>
2026-02-02 16:08:58 +0530

    Implemented the repositories

---
+++ FurnitureShop/FurnitureShop.Application/Interfaces/Services/IShippingAddressRepository.cs
@@ -0,0 +1,16 @@
+﻿using FurnitureShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Application.Interfaces.Services
+{
+    public interface IShippingAddressRepository
+    {
+        Task<ShippingAddress?> GetByUserIdAsync(Guid userId);
+        Task AddAsync(ShippingAddress address);
+        Task SaveChangesAsync();
+    }
+}
---
+++ FurnitureShop/FurnitureShop.Infrastructure/Repositories/ShippingAdderssRepository.cs
@@ -0,0 +1,38 @@
+﻿using FurnitureShop.Application.Interfaces.Services;
+using FurnitureShop.Domain.Entities;
+using FurnitureShop.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Infrastructure.Repositories
+{
+    public class ShippingAddressRepository : IShippingAddressRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShippingAddressRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ShippingAddress?> GetByUserIdAsync(Guid userId)
+        {
+            return await _context.ShippingAddresses
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+        }
+
+        public async Task AddAsync(ShippingAddress address)
+        {
+            _context.ShippingAddresses.Add(address);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+
+}
